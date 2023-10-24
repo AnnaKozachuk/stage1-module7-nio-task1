73b51115344c4224af257fb83f916b3ad040959e
FlyingElephant135 <46475082+FlyingElephant135@users.noreply.github.com>
2023-10-24 17:58:02 +0300

    commit

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -3,9 +3,6 @@ package com.epam.mjc.nio;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
