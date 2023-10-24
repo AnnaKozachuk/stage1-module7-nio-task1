4ce8d3b9ff11ba9be7812a04bd0b5984740f829d
FlyingElephant135 <46475082+FlyingElephant135@users.noreply.github.com>
2023-10-24 17:42:57 +0300

    commit

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,41 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public Profile getDataFromFile(File file) throws IOException {
+        Map<String, String> profileData;
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+            profileData = readProfileData(reader);
+        }
+        return createProfile(profileData);
+    }
+
+    private Map<String, String> readProfileData(BufferedReader reader) throws IOException {
+        Map<String, String> profileData = new HashMap<>();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] parts = line.split(": ");
+            if (parts.length == 2) {
+                profileData.put(parts[0], parts[1]);
+            }
+        }
+        return profileData;
+    }
+
+    private Profile createProfile(Map<String, String> profileData) {
+        String name = profileData.get("Name");
+        Integer age = Integer.parseInt(profileData.get("Age"));
+        String email = profileData.get("Email");
+        Long phone = Long.parseLong(profileData.get("Phone"));
+        return new Profile(name, age, email, phone);
     }
 }
